35e031b05ae6c40b755b9943ef1c6b5acb2868b4
janezhang650 <342291428@qq.com>
2022-08-04 10:52:47 +0800

    轮播图

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,78 @@
 <template>
-  <div id="index">
-    index
+  <div class="index">
+    <div class="container">
+      <div class="swiper-box">
+        <swiper :options="swiperOption">
+          <swiper-slide v-for="item in swiperList" :key="item.id">
+            <a :href="'/product/' + item.id">
+              <img :src="item.url" alt="">
+            </a>
+          </swiper-slide>
+          <div class="swiper-pagination"  slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+          <div class="swiper-scrollbar"   slot="scrollbar"></div>
+        </swiper>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
 export default {
-  name: 'index'
+  name: 'index',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        autoplay: true,
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        effect: 'fade',
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        }
+      },
+      swiperList: [
+        {
+          id: 1,
+          url: '/images/swiper1.jpeg'
+        },
+        {
+          id: 2,
+          url: '/images/swiper2.jpeg'
+        },
+        {
+          id: 3,
+          url: '/images/swiper3.jpg'
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .index {
+    .container{
+      .swiper-box {
+        height: 451px;
+        .swiper-container {
+          height: 100%;
+          img {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+</style>
